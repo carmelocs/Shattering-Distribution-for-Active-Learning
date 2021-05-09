3194de38fee493088fda0f0d367aab29d197ea26
Shuai CHENG <melo89dec@hotmail.com>
2021-05-09 21:14:30 +1000

    python

--- python/Demo.py
+++ python/Demo.py
@@ -1,13 +1,19 @@
 # Generated with SMOP  0.41-beta
 from libsmop import *
-# Demo.m
+import numpy as np
 
-    # clear
-    # clc
-    K=rbfkernel(data,data,1.8)
-# Demo.m:3
-    ID=Halving(K,400)
-# Demo.m:5
-    X=data(ID,arange())
-# Demo.m:7
-    SDAL(X,4)
+from Halving import Halving
+from SDAL import SDAL
+from sklearn.metrics.pairwise import rbf_kernel
+
+
+np.random.seed(0)
+data = np.random.rand(800, 2)
+
+K = rbf_kernel(data,data,1.8)
+
+ID = Halving(K,400)
+
+X = data(ID,arange())
+
+SDAL(X,4)
