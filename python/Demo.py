7721cb94f95f01899359e3da3a037a358985252a
Shuai CHENG <melo89dec@hotmail.com>
2021-05-18 21:49:44 +1000

    fix bugs

--- python/Demo.py
+++ python/Demo.py
@@ -18,46 +18,43 @@ warnings.filterwarnings("ignore",category=matplotlib.cbook.mplDeprecation)
 
 #     return K_xy
 
-def halving(K, m, candidate_index=None, lambda_=0.001):
+def halving(K, n_samples, candidate_index=None, lambda_=0.001):
     
-    n = K.shape[0]
+    n_data = K.shape[0]
     # print(f'number of data: {n}')
 
-    m = min(n, m)
+    n_samples = min(n_data, n_samples)
     # print(f'number of samples: {m}')
 
     if candidate_index is None:
-        candidate_index = np.array(range(n))
+        candidate_index = np.array(range(n_data))
     
     # print(f'candidate_index: {candidate_index}')
 
-    q = len(candidate_index)
+    n_query = len(candidate_index)
 
-    index = np.empty(m, dtype=int)
+    index = np.empty(n_samples, dtype=int)
     # print(f'number of index: {index.shape}')
 
-    print(f'Selecting {m} samples......')
-    for i in range(m):
-        score = np.zeros(q)
-        for j in range(q):
+    # print('Selecting samples......')
+    for i in range(n_samples):
+        score = np.zeros(n_query)
+        for j in range(n_query):
             if candidate_index[j] == -1:
                 continue
             else:
                 k = candidate_index[j]
-                # print(k)
                 score[j] = np.dot(K[k, :], K[:, k]) / (K[k, k] + lambda_)
         
         I = score.argmax()
-        # print(I)
         index[i] = candidate_index[I]
 
         candidate_index[I] = -1
         
         # update K
-        # K = K - np.dot(K[:, index[i]], K[index[i], :]) / (K[index[i], index[i]] + lambda_)
         K = K - K[:, index[i]][:, np.newaxis] @ K[index[i], :][np.newaxis, :] / (K[index[i], index[i]] + lambda_)
 
-    print('Done.\n')
+    # print('Done.\n')
     return index
 
 def number_density(data, center, radius):
